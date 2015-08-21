f04bb29029c351ac586b1c3498bba84b2ce09c32
luiz carlos silvestrini <luizsilvestrini15@hotmail.com>
2015-08-21 20:19:52 -0300

    Classe Produto

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
