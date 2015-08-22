6a71912e5bdd62b97e3ea55be7f6ecd56ddc0255
luiz carlos silvestrini <luizsilvestrini15@hotmail.com>
2015-08-21 21:55:17 -0300

    Inicio Vitrine

--- Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,11 @@ namespace Quiron.LojaVirtual.Dominio.Repositorio
     public class EfDbContext : DbContext
     {
         public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
     }
 }
